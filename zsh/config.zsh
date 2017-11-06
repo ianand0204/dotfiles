68bee6111d396cdd20b8ed6bb966cea8bc5a0492
ian_anderson <ian_anderson@apple.com>
2017-11-06 15:10:31 -0800

    more stuff

---
+++ zsh/config.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+export LSCOLORS='exfxcxdxbxegedabagacad'
+export CLICOLOR=true
+fpath=($DOTFILES/functions $fpath)
+
+autoload -U "$DOTFILES"/functions/*(:t)
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
