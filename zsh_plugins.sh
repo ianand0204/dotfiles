65b6f953374a972cce43492623083f10d6568bb3
ian_anderson <ian_anderson@apple.com>
2017-12-15 12:05:06 -0800

    more updates

--- zsh_plugins.sh
+++
@@ -1,3 +0,0 @@
-source /Users/ianderson3/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-source /Users/ianderson3/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-source /Users/ianderson3/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
